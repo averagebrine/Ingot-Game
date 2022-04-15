a87226589e3f383b0bba357a0d73bb1f14c0f7ee
NoMoreBrine <84639405+NoMoreBrine@users.noreply.github.com>
2022-04-15 10:36:26 -0400

    commented code
    copilot helped :D

--- Ingot Game/Assets/Scripts/Character/CharacterMovement.cs
+++ Ingot Game/Assets/Scripts/Character/CharacterMovement.cs
@@ -31,30 +31,29 @@ public class CharacterMovement : MonoBehaviour
 
     [Header("Misc")]
 
+    // components
     private Rigidbody2D rb;
+    private Animator animator;
     private float directionX;
     private bool sprint;
     private bool isFacingRight = true;
 
+    // logic
     private bool grounded;
     private bool jumpRequest;
     private bool crouchRequest;
     private bool crouching;
-
-    private Animator animator;
-    private float characterX;
-    private float characterY;
-    private bool characterGrounded;
-    private bool characterJumping;
     
     private void Awake()
     {
+        // initialize
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
+        // get input
         directionX = Input.GetAxisRaw("Horizontal");
 
         if(!crouching && grounded && Input.GetButtonDown("Jump")) jumpRequest = true;
@@ -65,14 +64,13 @@ public class CharacterMovement : MonoBehaviour
         if(!crouching && Input.GetButton("Sprint")) sprint = true;
         else sprint = false;
 
-        // unity tends to make the velocity a very small number instead of zero 
+
+        // wow, this is compact.
         if(Mathf.Abs(rb.velocity.x) < 0.01f) animator.SetFloat("VelocityX", 0f);
         else animator.SetFloat("VelocityX", Mathf.Abs(rb.velocity.x));
-
         if(Mathf.Abs(rb.velocity.y) < 0.01f) animator.SetFloat("VelocityY", 0f);
         else animator.SetFloat("VelocityY", rb.velocity.y);
 
-        // should this be done in FixedUpdate where the grounded bool is set?
         animator.SetBool("Grounded", grounded);
         animator.SetBool("Crouching", crouching);
     }
@@ -82,33 +80,27 @@ public class CharacterMovement : MonoBehaviour
         ApplyLinearSmooth();
         Move();
 
+        // boing
         if(!crouching && jumpRequest)
         {
             rb.AddForce(Vector2.up * jumpVelocity, ForceMode2D.Impulse);
             jumpRequest = false;
-        }
+        }       
         else
-        {
+        {   // this is where we make sure the character is grounded
             grounded = (Physics2D.OverlapBox(groundCheckPosition.position, boxSize, 0f, whatIsGround) != null);
         }
 
-        // higher gravity when falling 
-        // velocity set to 0 if jump is cancelled --change that maybe
-        if(rb.velocity.y < 0)
-        {
-            rb.gravityScale = defaultGravity * fallMultiplier;
-        }
-        else if(rb.velocity.y > 0 && !Input.GetButton("Jump"))
-        {
-            rb.gravityScale = defaultGravity * lowJumpMultiplier;
-            // rb.velocity = new Vector2(rb.velocity.x, 0f);
-        }
-        else
-        {
-            rb.gravityScale = defaultGravity;
-        }
+
+        // this isn't the best way to do this, but it works
+        // if the lowJumpMultiplier is too high, the character might fall too quickly :/
+        if(rb.velocity.y < 0) rb.gravityScale = defaultGravity * fallMultiplier;
+        else if(rb.velocity.y > 0 && !Input.GetButton("Jump")) rb.gravityScale = defaultGravity * lowJumpMultiplier;
+        else rb.gravityScale = defaultGravity;
 
 
+        // sneak
+        // compact if statements are hard to read
         if(grounded && crouchRequest) crouching = true;
         else if(grounded && !crouchRequest && Physics2D.OverlapBox(ceilingCheckPosition.position, ceilingCheckSize, 0f, whatIsGround)) crouching = true;
         else crouching = false;
@@ -139,6 +131,7 @@ public class CharacterMovement : MonoBehaviour
             rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * maxSpeed, rb.velocity.y);
         }
 
+
         // move character
         if(sprint) rb.AddForce(new Vector2(directionX, 0f) * (accelerationSpeed * sprintAccelerationMultiplier));
         else rb.AddForce(new Vector2(directionX, 0f) * accelerationSpeed);
@@ -149,15 +142,16 @@ public class CharacterMovement : MonoBehaviour
         else if(directionX < 0 && isFacingRight) Flip();
     }
 
-    // slow the character down if on the ground and not trying to move
     private void ApplyLinearSmooth()
     {
+        // this is self explanatory, right?
         if(grounded && Mathf.Abs(Input.GetAxisRaw("Horizontal")) < 0.01f) rb.velocity = new Vector2(rb.velocity.x * speedDecay, rb.velocity.y);
         if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) < 0.01f && Mathf.Abs(rb.velocity.x) < minimumVelocity && Mathf.Abs(rb.velocity.x) > 0.01f) rb.velocity = new Vector2(0f, rb.velocity.y);
     }
 
     private void Flip()
     {
+        // confusing math :D
         isFacingRight = !isFacingRight;
 
 		Vector3 theScale = transform.localScale;
--- Ingot Game/Assets/Scripts/Core/GameManager.cs
+++ Ingot Game/Assets/Scripts/Core/GameManager.cs
@@ -4,15 +4,17 @@ using System.Collections;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private Sprite coconut;
+
     void Awake()
     {
-        Application.OpenURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+        // most important code in the game
         if(coconut == null) Application.Quit();
     }
+
     void Update()
     {
-        // copilot wrote all of this for me
-        if (Input.GetKeyDown(KeyCode.Escape))
+        // should probably use the input manager
+        if (Input.GetKeyDown(KeyCode.Escape) && Input.GetKey(KeyCode.LeftControl))
         {
             Application.Quit();
             Debug.Log("Quit");
